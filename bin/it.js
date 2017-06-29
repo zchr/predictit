08023c7f40f313a3e90490ad6159f10d63cc1a5b
Zach Schnell <zeschnell@gmail.com>
2017-06-28 23:41:21 -0400

    call firebase thing

--- bin/it.js
+++ bin/it.js
@@ -1,6 +1,36 @@
 const axios = require('axios');
+const predictit = require('predict-it');
 
-axios.get('https://predictit-history.firebaseio-demo.com/')
-.then(function (response) {
-  console.log(response);
+const getAll = new Promise((resolve, reject) => {
+  const markets = predictit.all();
+  resolve(markets);
 })
+
+
+getAll.then((markets) => {
+  console.log(markets.length)
+  markets.forEach((market) => {
+    market.Contracts.forEach((c) => {
+      const attr =  {
+        dateEnd: c.DateEnd,
+        open: c.Status == 'Open'
+      }
+      const price = {
+        date: new Date(),
+        lastTradePrice: c.LastTradePrice,
+        bestBuyYesCost: c.BestBuyYesCost,
+        bestBuyNoCost: c.BestBuyNoCost,
+        bestSellYesCost: c.BestSellYesCost,
+        bestSellNoCost: c.BestSellNoCost
+      }
+      setTimeout(() => {
+        axios.put(`https://predictit-history.firebaseio.com/${market.ID}/${c.ID}.json`, attr,
+          { params: { auth: process.env.firebase } }).then(() => {
+          axios.post(`https://predictit-history.firebaseio.com/${market.ID}/${c.ID}/prices.json`, price,
+          { params: { auth: process.env.firebase } });
+        });
+      }, Math.random()*40000);
+
+    });
+  });
+});
