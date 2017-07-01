d00df17280456ff3f34cfa734d1c16f3ba96c097
Zach Schnell <zeschnell@gmail.com>
2017-07-01 17:57:41 -0400

    add charts

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const axios = require('axios')
+const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/data', function(request, response) {
+  return axios.get('https://predictit-history.firebaseio.com/.json',
+    { params: { auth: 'QLFxLVukRuEsxWyk1lOINdV0CNQXQ9du4JExPt5f' } })
+  .then((d) => {
+    response.send(JSON.stringify(d.data));
+  })
+  .catch(e => console.log(e));
 })
 
 app.listen(app.get('port'), function() {
